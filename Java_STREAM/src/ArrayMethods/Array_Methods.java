344d0f8d0266619e2abda20c87b403c26dc18685
Joseph Walker <96276860+J0311@users.noreply.github.com>
2022-07-27 19:22:01 -0400

    Update Array_Methods.java

--- Java_STREAM/src/ArrayMethods/Array_Methods.java
+++ Java_STREAM/src/ArrayMethods/Array_Methods.java
@@ -147,7 +147,7 @@ public class Array_Methods {
 		// take your integer array...turn it into an integer STREAM
 		// Store that integer stream into a variable: 
 		
-		int [] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+		int [] numbers = {1,2,3,4,5,5,6,7,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 		
 		IntStream is = Arrays.stream(numbers);
 		
